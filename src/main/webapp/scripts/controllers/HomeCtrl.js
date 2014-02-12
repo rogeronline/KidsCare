cfd6f75da41155bdb1867d641f976c1fdef24621
Johnny Zong <johnny.zong@sap.com>
2014-02-12 09:48:08 +0800

    rough tree

--- src/main/webapp/scripts/controllers/HomeCtrl.js
+++ src/main/webapp/scripts/controllers/HomeCtrl.js
@@ -3,6 +3,23 @@
 app.controller('HomeCtrl', ['$scope', 'dataStorage', 'channel', 'dataService',
 function($scope, dataStorage, channel, dataService) {
 
+    $scope.$on('$viewContentLoaded', function() {
+        var pic = $("#pic")[0];
+        var picUrls = ["bg", "day-number", "tree", "tree-trunk"];
+        var cursor = 0;
+        pic.addEventListener("webkitAnimationEnd", function(event) {
+            var bg = "url('images/" + picUrls[cursor++ % 4] + ".png') no-repeat";
+            $(pic).css({
+                "background": bg,
+                "backgroundSize": "contain"
+            });
+            $(pic).removeClass("rotate").removeClass("normal");
+            setTimeout(function() {
+                $(pic).addClass("rotate").addClass("normal");
+            }, 1);
+        });
+    });
+
     function update() {
         console.log('update view');
     }

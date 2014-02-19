1d820d8cc23349756a4a079f8ce390bb48c6855e
Johnny Zong <johnny.zong@sap.com>
2014-02-19 17:41:08 +0800

    canvas tree-search function

--- src/main/webapp/scripts/controllers/HomeCtrl.js
+++ src/main/webapp/scripts/controllers/HomeCtrl.js
@@ -7,6 +7,7 @@ function($scope, dataStorage, channel, dataService) {
         drawTreebody();
         picAnimate();
         timeListEventsHandler();
+        searchEventHandler();
     });
 
     $scope.dayChosen = function() {
@@ -21,8 +22,7 @@ function($scope, dataStorage, channel, dataService) {
                       [42, 434], [142, 434], [242, 434], [342, 434], [442, 434], [542, 434], [642, 434],
                       [0, 334], [100, 334], [200, 334], [300, 334], [400, 334], [500, 334], [600, 334],
                       [0, 234], [100, 234], [200, 234], [300, 234], [400, 234], [500, 234], [600, 234],
-                      [42, 134], [142, 134], [242, 134], [342, 134], [442, 134], [542, 134], [642, 134],
-                      [242, 34], [342, 34], [442, 34]];
+                      [42, 134], [142, 134], [242, 134]];
         var i = 0, len = posArr.length;
         var interval = setInterval(function() {
             var pos = posArr[i++];
@@ -36,9 +36,13 @@ function($scope, dataStorage, channel, dataService) {
                     ctx.arc(pos[0], pos[1], 50, 2 * Math.PI, 0, true);
                     ctx.fill();
                     pos.opacity += 0.05;
-                    if (pos.opacity < 1) {
+                    if (pos.opacity < 0.5) {
                         ctx.fillStyle = 'rgba(0, 0, 255,' + pos.opacity + ')';
                         fadeIn();
+                    } else {
+                        ctx.font = 'bold 16pt sans-serif';
+                        ctx.fillStyle = '#aaa';
+                        ctx.fillText('#aaa', pos[0], pos[1]);
                     }
                 }, 50);
             }
@@ -49,27 +53,79 @@ function($scope, dataStorage, channel, dataService) {
                 canvasEventHandler();
             }
         }, 100);
+
+        //insertImage
+        var img = new Image();
+        img.onload = function() {
+          drawImage(this);
+        };
+        img.src = './images/leaf1.png';
+
+        function drawImage(imageObj) {
+            ctx.save();
+            var x = 200;
+            var y = 50;
+
+            ctx.globalAlpha = 0.1;
+            var alphas = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
+            var cursor = 0;
+            ctx.drawImage(imageObj, x, y);
+            var fadeIn = function() {
+                setTimeout(function() {
+                    ctx.globalAlpha = alphas[cursor++];
+                    ctx.drawImage(imageObj, x, y);
+                    if (cursor <= 8) {
+                        fadeIn();
+                    }
+                }, 200);
+            };
+            fadeIn();
+            ctx.restore();
+        }
+    }
+
+    function searchEventHandler() {
+        $('#food_search_icon').click(function(e) {
+            $(this).fadeOut();
+            $("#food_search_box").fadeIn();
+            $scope.canvas.onmousemove = null;
+        });
+        $("#food_search_btn").click(function() {
+            $("#food_search_box").fadeOut();
+            $scope.canvas.onmousemove = canvasMove;
+        });
+    }
+
+    function canvasMove(e) {
+        var canvas = $scope.canvas;
+        var bb = canvas.getBoundingClientRect();
+        var x = (e.clientX - bb.left) * (canvas.width / bb.width);
+        var y = (e.clientY - bb.top) * (canvas.height / bb.height);
+        var i = 0, len = $scope.posArr.length;
+        for (i; i < len; i++) {
+            var pos = $scope.posArr[i];
+            if ((Math.pow(x - pos[0], 2) + Math.pow(y - pos[1], 2)) <= Math.pow(50, 2)) {
+                canvas.style.cursor = 'pointer';
+                $("#food_search_icon").fadeIn().css({
+                    left: e.clientX - 50,
+                    top: e.clientY - 50
+                }).data('circleId', i);
+                break;
+            }
+        }
+        if (i >= len) {
+            canvas.style.cursor = 'default';
+            $(".search-icon").fadeOut();
+        }
     }
 
     function canvasEventHandler() {
         var canvas = $scope.canvas;
-        canvas.onmousemove = function(e) {
-            var bb = canvas.getBoundingClientRect();
-            var x = (e.clientX - bb.left) * (canvas.width / bb.width);
-            var y = (e.clientY - bb.top) * (canvas.height / bb.height);
-            var pixels = canvas.getContext('2d').getImageData(x, y, 1, 1);
-            var i = 3, len = pixels.data.length;
-            for (i; i < len; i += 4) {
-                if (pixels.data[i] != 0 ) {
-                    canvas.style.cursor = 'pointer';
-                    break;
-                }
-            }
-            if (i >= len) {
-                canvas.style.cursor = 'default';
-            }
+        canvas.onmousemove = canvasMove;
+        canvas.onmouseout = function(e) {
+            $(".searchIcon").fadeOut();
         };
-        canvas.onclick = function(e) {
+        /*canvas.onclick = function(e) {
             var bb = canvas.getBoundingClientRect();
             var x = (e.clientX - bb.left) * (canvas.width / bb.width);
             var y = (e.clientY - bb.top) * (canvas.height / bb.height);
@@ -80,7 +136,7 @@ function($scope, dataStorage, channel, dataService) {
                     alert("circle" + i);
                 }
             }
-        };
+        };*/
     }
 
     function picAnimate() {

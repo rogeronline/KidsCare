df300b3c6b47c08bee651102f20b9d90d6fadd22
wanderyt <wanderyt@hotmail.com>
2014-02-19 09:54:26 +0800

    function extraction in MilkDetailCtrl.js.

--- src/main/webapp/scripts/controllers/MilkDetailCtrl.js
+++ src/main/webapp/scripts/controllers/MilkDetailCtrl.js
@@ -8,11 +8,12 @@ function($scope, dataStorage, channel, dataService) {
         drawBadKeyGraph();
     });
 
+    var marksAttr = {fill: "#444", stroke: "none"};
+
     function drawGoodKeyGraph() {
         var r = Raphael("good", 500, 500),
             R = 180,
-            param = {stroke: "#fff", "stroke-width": 15},
-            marksAttr = {fill: "#444", stroke: "none"};
+            param = {stroke: "#fff", "stroke-width": 15};
 
         r.customAttributes.arc = function (value, total, R) {
             var alpha = 360 / total * value,
@@ -24,44 +25,21 @@ function($scope, dataStorage, channel, dataService) {
             return {path: path, stroke: color};
         };
 
-        drawMarks(R);
+        drawMarks(r, R);
         var fir = r.path().attr(param).attr({arc: [0, 60, R]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(R);
+        drawMarks(r, R);
         var sec = r.path().attr(param).attr({arc: [0, 60, R]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(R);
+        drawMarks(r, R);
         var thi = r.path().attr(param).attr({arc: [0, 12, R]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(R);
+        drawMarks(r, R);
         var fou = r.path().attr(param).attr({arc: [0, 31, R]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(R);
+        drawMarks(r, R);
         var fiv = r.path().attr(param).attr({arc: [0, 12, R]}).attr({opacity: 0.6});
-
-        function drawMarks(R) {
-            var pathParam = {stroke: "#fff", "stroke-width": 1};
-            var color = "hsb(".concat(Math.round(R) / 200, ", 1, .75)"),
-                out = r.set();
-            var alpha = 360 / 12 * 8,
-                a = (90 - alpha) * Math.PI / 180,
-                x = 250 + R * Math.cos(a),
-                y = 250 - R * Math.sin(a);
-            out.push(r.path().attr(pathParam).attr({arc: [8, 12, R]}));
-            out.push(r.circle(x, y, 5).attr(marksAttr));
-            out.push(r.text(x, y - 20, "40%"));
-
-            var alpha = 360 / 12 * 12,
-                a = (90 - alpha) * Math.PI / 180,
-                x = 250 + R * Math.cos(a),
-                y = 250 - R * Math.sin(a);
-            out.push(r.text(x - 100, y, "Allergic"));
-            return out;
-        }
-
-        function updateVal(value, total, R, hand) {
-            hand.animate({arc: [value, total, R]}, 900, "elastic");
-        }
+        r.circle(250, 250, 60).attr({stroke: "none", fill: Raphael.hsb2rgb(15 / 200, 1, .75).hex});
 
         updateVal(60, 100, 180, fir);
         updateVal(50, 100, 155, sec);
@@ -70,11 +48,34 @@ function($scope, dataStorage, channel, dataService) {
         updateVal(27, 100, 80, fiv);
     }
 
+    function drawMarks(r, R, lineColor, infoColor) {
+        var pathParam = {stroke: "#fff", "stroke-width": 1};
+        var color = "hsb(".concat(Math.round(R) / 200, ", 1, .75)"),
+            out = r.set();
+        var alpha = 360 / 12 * 8,
+            a = (90 - alpha) * Math.PI / 180,
+            x = 250 + R * Math.cos(a),
+            y = 250 - R * Math.sin(a);
+        out.push(r.path().attr(pathParam).attr({arc: [8, 12, R]}));
+        out.push(r.circle(x, y, 5).attr(marksAttr));
+        out.push(r.text(x, y - 20, "40%"));
+
+        var alpha = 360 / 12 * 12,
+            a = (90 - alpha) * Math.PI / 180,
+            x = 250 + R * Math.cos(a),
+            y = 250 - R * Math.sin(a);
+        out.push(r.text(x - 100, y, "Allergic"));
+        return out;
+    }
+
+    function updateVal(value, total, R, hand) {
+        hand.animate({arc: [value, total, R]}, 900, "elastic");
+    }
+
     function drawBadKeyGraph() {
         var r = Raphael("bad", 500, 500),
             R = 180,
-            param = {stroke: "#fff", "stroke-width": 15},
-            marksAttr = {fill: "#444", stroke: "none"};
+            param = {stroke: "#fff", "stroke-width": 15};
 
         r.customAttributes.arc = function (value, total, R) {
             var alpha = 360 / total * value,
@@ -86,44 +87,20 @@ function($scope, dataStorage, channel, dataService) {
             return {path: path, stroke: color};
         };
 
-        drawMarks(R);
+        drawMarks(r, R);
         var fir = r.path().attr(param).attr({arc: [0, 60, R]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(R);
+        drawMarks(r, R);
         var sec = r.path().attr(param).attr({arc: [0, 60, R]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(R);
+        drawMarks(r, R);
         var thi = r.path().attr(param).attr({arc: [0, 12, R]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(R);
+        drawMarks(r, R);
         var fou = r.path().attr(param).attr({arc: [0, 31, R]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(R);
+        drawMarks(r, R);
         var fiv = r.path().attr(param).attr({arc: [0, 12, R]}).attr({opacity: 0.6});
-
-        function drawMarks(R) {
-            var pathParam = {stroke: "#fff", "stroke-width": 1};
-            var color = "hsb(".concat(Math.round(R) / 200, ", 1, .75)"),
-                out = r.set();
-            var alpha = 360 / 12 * 8,
-                a = (90 - alpha) * Math.PI / 180,
-                x = 250 + R * Math.cos(a),
-                y = 250 - R * Math.sin(a);
-            out.push(r.path().attr(pathParam).attr({arc: [8, 12, R]}));
-            out.push(r.circle(x, y, 5).attr(marksAttr));
-            out.push(r.text(x, y - 20, "40%"));
-
-            var alpha = 360 / 12 * 12,
-                a = (90 - alpha) * Math.PI / 180,
-                x = 250 + R * Math.cos(a),
-                y = 250 - R * Math.sin(a);
-            out.push(r.text(x - 100, y, "Allergic"));
-            return out;
-        }
-
-        function updateVal(value, total, R, hand) {
-            hand.animate({arc: [value, total, R]}, 900, "elastic");
-        }
 
         updateVal(60, 100, 180, fir);
         updateVal(50, 100, 155, sec);

cf9f92dd52be48b5e9b93a2a8b37949895704346
Roger Li <roger.li@sap.com>
2014-02-20 14:38:42 +0800

    update

---
+++ src/main/java/com/sap/cdsp/kidscare/service/BrandsAPI.java
@@ -0,0 +1,36 @@
+package com.sap.cdsp.kidscare.service;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BrandsAPI extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//String path = Contants.getJsonFilePath("cities",request.getSession().getServletContext());
+		
+		response.setCharacterEncoding("UTF-8");
+		Writer writer = response.getWriter();
+		//response.setContentType("application/json");
+		writer.write("test here");
+		
+	}
+}

0846555d390bf47adddaf1844aa63721571dfd98
wanderyt <wanderyt@hotmail.com>
2014-02-21 15:09:40 +0800

    Add questionnaire modal.

--- src/main/webapp/scripts/controllers/MedicalDetailCtrl.js
+++ src/main/webapp/scripts/controllers/MedicalDetailCtrl.js
@@ -1,24 +1,15 @@
 'use strict';
 
-app.controller('TopicDetailCtrl', ['$scope', 'dataStorage', 'channel', 'dataService',
+app.controller('MedicalDetailCtrl', ['$scope', 'dataStorage', 'channel', 'dataService',
 function($scope, dataStorage, channel, dataService) {
 
-    $('#post_detail').on('shown.bs.modal', function (e) {
+    /*$('#post_detail').on('shown.bs.modal', function (e) {
         var params = {};
         dataService.getPost(params, function(data) {
             data.keywords = data.keywords.join(", ");
             $scope.topic = data;
 
-            $('#post_detail').transition({
-                duration: 500,
-                perspective: '1100px',
-                rotate3d: '1,1,0,360deg'
-            }, function(){
-                //reset the transform property
-                $(this).css('transform', '');
-            });
-
-            /*dataService.getRelatedPostsByTopic(params, function(data) {
+            dataService.getRelatedPostsByTopic(params, function(data) {
                 $scope.posts = data.results;
                 $('#post_detail').transition({
                     duration: 500,
@@ -28,9 +19,9 @@ function($scope, dataStorage, channel, dataService) {
                     //reset the transform property
                     $(this).css('transform', '');
                 });
-            });*/
+            })
         });
-    });
+    });*/
 
     //--------------------------------------------------------------------------
     // bootstrap

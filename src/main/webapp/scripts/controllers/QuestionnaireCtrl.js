ab63be85f2838de1bdbffdd333af0064c6aa9b96
Rita Zhou <yanzhou1224@gmail.com>
2014-02-21 22:06:44 +0800

    add pop up

---
+++ src/main/webapp/scripts/controllers/QuestionnaireCtrl.js
@@ -0,0 +1,35 @@
+'use strict';
+
+app.controller('QuestionnaireCtrl', ['$scope', 'dataStorage', 'channel', 'dataService',
+function($scope, dataStorage, channel, dataService) {
+    $scope.nextClick = function(){
+        if($scope.number == 1) {
+            $scope.number = 2;
+            $scope.question = 'Does baby have a fever?';
+            return;
+        }
+        if($scope.number == 2) {
+            $scope.number = 3;
+            $scope.question = 'Does  the baby feed by formula?';
+            return;
+        }
+        if($scope.number = 3){
+            return;
+            //close the popup and go to disease page.
+        }
+    };
+    //--------------------------------------------------------------------------
+    // bootstrap
+    //--------------------------------------------------------------------------
+
+    function init() {
+    }
+
+    function refresh() {
+        $scope.number = 1;
+        $scope.question = 'Are the red pots  mainly on the head and face?';
+    }
+
+    init();
+    refresh();
+}]);

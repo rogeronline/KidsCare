ab6e305bf10d3aed6db83ccf9bdd6f6ab8e2c865
wanderyt <wanderyt@hotmail.com>
2014-02-22 16:05:23 +0800

    Milk detail modal.

--- src/main/webapp/scripts/controllers/MilkDetailCtrl.js
+++ src/main/webapp/scripts/controllers/MilkDetailCtrl.js
@@ -36,106 +36,127 @@ function($scope, dataStorage, channel, dataService) {
         $('#bad').text("");
     });
 
-    var marksAttr = {fill: "#444", stroke: "none"};
-
     function drawGoodKeyGraph(data) {
         var r = Raphael("good", 500, 500),
             R = 180,
-            param = {stroke: "#fff", "stroke-width": 15};
+            param = {stroke: "#00c4ed", "stroke-width": 22};
+        var titleAttr = {
+            "font-size" : "30px",
+            "font-weight" : "bold",
+            "text-anchor" : "middle"
+        };
 
-        r.customAttributes.arc = function (value, total, R) {
+        r.customAttributes.arc = function (value, total, R, lineColor) {
             var alpha = 360 / total * value,
                 a = (90 - alpha) * Math.PI / 180,
                 x = 250 + R * Math.cos(a),
                 y = 250 - R * Math.sin(a),
-                color = "hsb(".concat(Math.round(R) / 200, ",", value / total, ", .75)");
+                //color = "hsb(".concat(Math.round(R) / 200, ",", value / total, ", .75)");
+                //color = "#00c4ed";
+                color = lineColor;
             var path = [["M", 250, 250 - R], ["A", R, R, 0, +(alpha > 180), 1, x, y]];
             return {path: path, stroke: color};
         };
 
-        drawMarks(r, R, data[0]);
-        var fir = r.path().attr(param).attr({arc: [0, 60, R]}).attr({opacity: 0.6});
+        drawMarks(r, R, data[0], "#00c4ed");
+        var fir = r.path().attr(param).attr({arc: [0, 60, R, "#00c4ed"]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(r, R, data[1]);
-        var sec = r.path().attr(param).attr({arc: [0, 60, R]}).attr({opacity: 0.6});
+        drawMarks(r, R, data[1], "#00c4ed");
+        var sec = r.path().attr(param).attr({arc: [0, 60, R, "#00c4ed"]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(r, R, data[2]);
-        var thi = r.path().attr(param).attr({arc: [0, 12, R]}).attr({opacity: 0.6});
+        drawMarks(r, R, data[2], "#00c4ed");
+        var thi = r.path().attr(param).attr({arc: [0, 12, R, "#00c4ed"]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(r, R, data[3]);
-        var fou = r.path().attr(param).attr({arc: [0, 31, R]}).attr({opacity: 0.6});
+        drawMarks(r, R, data[3], "#00c4ed");
+        var fou = r.path().attr(param).attr({arc: [0, 31, R, "#00c4ed"]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(r, R, data[4]);
-        var fiv = r.path().attr(param).attr({arc: [0, 12, R]}).attr({opacity: 0.6});
-        r.circle(250, 250, 60).attr({stroke: "none", fill: Raphael.hsb2rgb(15 / 200, 1, .75).hex});
+        drawMarks(r, R, data[4], "#00c4ed");
+        var fiv = r.path().attr(param).attr({arc: [0, 12, R, "#00c4ed"]}).attr({opacity: 0.6});
+        //r.circle(250, 250, 60).attr({stroke: "none", fill: Raphael.hsb2rgb(15 / 200, 1, .75).hex});
+        r.image("images/pros_bg.png", 190, 190, 120, 120);
+        r.text(250, 250, "Pros").attr(titleAttr);
 
-        updateVal(data[0].value / 3 * 2, 100, 180, fir);
-        updateVal(data[1].value / 3 * 2, 100, 155, sec);
-        updateVal(data[2].value / 3 * 2, 100, 130, thi);
-        updateVal(data[3].value / 3 * 2, 100, 105, fou);
-        updateVal(data[4].value / 3 * 2, 100, 80, fiv);
+        updateVal(data[0].value / 3 * 2, 100, 180, fir, "#16cce9");
+        updateVal(data[1].value / 3 * 2, 100, 155, sec, "#53d2e7");
+        updateVal(data[2].value / 3 * 2, 100, 130, thi, "#7bdae8");
+        updateVal(data[3].value / 3 * 2, 100, 105, fou, "#89dde1");
+        updateVal(data[4].value / 3 * 2, 100, 80, fiv, "#9de2e7");
     }
 
     function drawBadKeyGraph(data) {
         var r = Raphael("bad", 500, 500),
             R = 180,
-            param = {stroke: "#fff", "stroke-width": 15};
+            param = {stroke: "#fb915c", "stroke-width": 22};
+        var titleAttr = {
+            "font-size" : "30px",
+            "font-weight" : "bold",
+            "text-anchor" : "middle"
+        };
 
-        r.customAttributes.arc = function (value, total, R) {
+        r.customAttributes.arc = function (value, total, R, lineColor) {
             var alpha = 360 / total * value,
                 a = (90 - alpha) * Math.PI / 180,
                 x = 250 + R * Math.cos(a),
                 y = 250 - R * Math.sin(a),
-                color = "hsb(".concat(Math.round(R) / 200, ",", value / total, ", .75)");
+                //color = "hsb(".concat(Math.round(R) / 200, ",", value / total, ", .75)");
+                //color = "#fb915c";
+                color = lineColor;
             var path = [["M", 250, 250 - R], ["A", R, R, 0, +(alpha > 180), 1, x, y]];
             return {path: path, stroke: color};
         };
 
-        drawMarks(r, R, data[0]);
-        var fir = r.path().attr(param).attr({arc: [0, 60, R]}).attr({opacity: 0.6});
+        drawMarks(r, R, data[0], "#ff630f");
+        var fir = r.path().attr(param).attr({arc: [0, 60, R, "#ff630f"]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(r, R, data[1]);
-        var sec = r.path().attr(param).attr({arc: [0, 60, R]}).attr({opacity: 0.6});
+        drawMarks(r, R, data[1], "#ff630f");
+        var sec = r.path().attr(param).attr({arc: [0, 60, R, "#ff630f"]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(r, R, data[2]);
-        var thi = r.path().attr(param).attr({arc: [0, 12, R]}).attr({opacity: 0.6});
+        drawMarks(r, R, data[2], "#ff630f");
+        var thi = r.path().attr(param).attr({arc: [0, 12, R, "#ff630f"]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(r, R, data[3]);
-        var fou = r.path().attr(param).attr({arc: [0, 31, R]}).attr({opacity: 0.6});
+        drawMarks(r, R, data[3], "#ff630f");
+        var fou = r.path().attr(param).attr({arc: [0, 31, R, "#ff630f"]}).attr({opacity: 0.6});
         R -= 25;
-        drawMarks(r, R, data[4]);
-        var fiv = r.path().attr(param).attr({arc: [0, 12, R]}).attr({opacity: 0.6});
-        r.circle(250, 250, 60).attr({stroke: "none", fill: Raphael.hsb2rgb(15 / 200, 1, .75).hex});
+        drawMarks(r, R, data[4], "#ff630f");
+        var fiv = r.path().attr(param).attr({arc: [0, 12, R, "#ff630f"]}).attr({opacity: 0.6});
+        r.image("images/pros_bg.png", 190, 190, 120, 120);
+        r.text(250, 250, "Cons").attr(titleAttr);
 
-        updateVal(data[0].value / 3 * 2, 100, 180, fir);
-        updateVal(data[1].value / 3 * 2, 100, 155, sec);
-        updateVal(data[2].value / 3 * 2, 100, 130, thi);
-        updateVal(data[3].value / 3 * 2, 100, 105, fou);
-        updateVal(data[4].value / 3 * 2, 100, 80, fiv);
+        updateVal(data[0].value / 3 * 2, 100, 180, fir, "#f9b36b");
+        updateVal(data[1].value / 3 * 2, 100, 155, sec, "#ffbb71");
+        updateVal(data[2].value / 3 * 2, 100, 130, thi, "#f8b77b");
+        updateVal(data[3].value / 3 * 2, 100, 105, fou, "#f9ba77");
+        updateVal(data[4].value / 3 * 2, 100, 80, fiv, "#f1c590");
     }
 
-    function drawMarks(r, R, data, lineColor, infoColor) {
-        var pathParam = {stroke: "#fff", "stroke-width": 1};
-        var color = "hsb(".concat(Math.round(R) / 200, ", 1, .75)"),
-            out = r.set();
+    function drawMarks(r, R, data, lineColor) {
+        var pathParam = {stroke: "#0000ff", "stroke-width": 1};
+        var marksAttr = {fill: lineColor, stroke: "none"};
+        var percentAttr = {
+            "font-size" : "14px",
+            "font-weight" : "bold",
+            "color" : lineColor,
+            "text-anchor" : "end"
+        };
+        var out = r.set();
         var alpha = 360 / 12 * 8,
             a = (90 - alpha) * Math.PI / 180,
             x = 250 + R * Math.cos(a),
             y = 250 - R * Math.sin(a);
-        out.push(r.path().attr(pathParam).attr({arc: [8, 12, R]}));
+        out.push(r.path().attr(pathParam).attr({arc: [8, 12, R, lineColor]}));
         out.push(r.circle(x, y, 5).attr(marksAttr));
-        out.push(r.text(x, y - 20, data.value + "%"));
+        out.push(r.text(x, y - 19, data.value + "%").attr(percentAttr));
 
         var alpha = 360 / 12 * 12,
             a = (90 - alpha) * Math.PI / 180,
             x = 250 + R * Math.cos(a),
             y = 250 - R * Math.sin(a);
-        out.push(r.text(x - 100, y, data.name));
+        out.push(r.text(x - 20, y, data.name).attr(percentAttr));
         return out;
     }
 
-    function updateVal(value, total, R, hand) {
-        hand.animate({arc: [value, total, R]}, 900, "linear");
+    function updateVal(value, total, R, hand, lineColor) {
+        hand.animate({arc: [value, total, R, lineColor]}, 900, "linear");
     }
 
     function sortByPercent(data) {
